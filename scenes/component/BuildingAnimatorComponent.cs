25e2eb27a844316affac244e8d8dc2cbd5b80a17
Toby <ttobygeo44@gmail.com>
2025-03-08 11:17:42 -0700

    Animating Building Placement

---
+++ scenes/component/BuildingAnimatorComponent.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using Godot;
+
+namespace Game.Component;
+
+public partial class BuildingAnimatorComponent : Node2D
+{
+    private Tween activeTween;
+    private Node2D animationRootNode;
+
+    public override void _Ready()
+    {
+        SetUpNodes();
+        PlayInAnimation();
+    }
+
+    public void PlayInAnimation()
+    {
+        if (animationRootNode == null) return;
+
+        // ensures there isnt more than one of the same tween active.
+        if (activeTween != null && activeTween.IsValid())
+        {
+            activeTween.Kill();
+        }
+        
+        activeTween = CreateTween();
+        //This animates the "fall" of the building starting from 128 pixels up
+        activeTween
+            .TweenProperty(animationRootNode, "position", Vector2.Zero, .3)
+            .SetTrans(Tween.TransitionType.Quad)
+            .SetEase(Tween.EaseType.In)
+            .From(Vector2.Up * 128);
+        activeTween
+            .TweenProperty(animationRootNode, "position", Vector2.Up * 16, .1)
+            .SetTrans(Tween.TransitionType.Quad)
+            .SetEase(Tween.EaseType.Out);
+        activeTween
+            .TweenProperty(animationRootNode, "position", Vector2.Zero, .1)
+            .SetTrans(Tween.TransitionType.Quad)
+            .SetEase(Tween.EaseType.In);
+    }
+
+    //This creates a new node so the Y sort is anchored and the building "fall" animation plays without flickering.
+    private void SetUpNodes()
+    {
+        var spriteNode = GetChildren().FirstOrDefault() as Node2D;
+        if (spriteNode == null)
+        {
+            return;
+        }
+        // Removes and adds sprites to be in the proper order and position within the tree.
+        RemoveChild(spriteNode);
+        Position = new Vector2(Position.X, spriteNode.Position.Y);
+        animationRootNode = new Node2D();
+        AddChild(animationRootNode);
+        animationRootNode.AddChild(spriteNode);
+        spriteNode.Position = new Vector2(spriteNode.Position.X, 0);
+    }
+}

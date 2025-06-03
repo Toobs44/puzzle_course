e1d39d2c3fbbe096cae081f160f871cee0fdfb11
Toby <ttobygeo44@gmail.com>
2025-06-03 12:33:02 -0600

    Window scaling configuration

--- scenes/ui/LevelSelectSection.cs
+++ scenes/ui/LevelSelectSection.cs
@@ -1,3 +1,4 @@
+using Game.Autoload;
 using Game.Resources.Level;
 using Godot;
 
@@ -11,6 +12,7 @@ public partial class LevelSelectSection : PanelContainer
 	private Button button;
 	private Label resourceCountLabel;
 	private Label levelNumberLabel;
+	private TextureRect completedIndicator;
 	private int levelIndex;
 
     public override void _Ready()
@@ -18,13 +20,15 @@ public partial class LevelSelectSection : PanelContainer
         button = GetNode<Button>("%Button");
 		resourceCountLabel = GetNode<Label>("%ResourceCountLabel");
 		levelNumberLabel = GetNode<Label>("%LevelNumberLabel");
+		completedIndicator = GetNode<TextureRect>("%CompletedIndicator");
 
 		button.Pressed += OnButtonPressed;
     }
 
-    public void SetLevelDefinition(LevelDefinitionResource levelDefinitionResource)
+	public void SetLevelDefinition(LevelDefinitionResource levelDefinitionResource)
 	{
 		resourceCountLabel.Text = levelDefinitionResource.StartingResourceCount.ToString();
+		completedIndicator.Visible = SaveManager.IsLevelCompleted(levelDefinitionResource.Id);
 
 	}
 
